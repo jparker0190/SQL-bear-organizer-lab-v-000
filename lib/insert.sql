44df01be98b4fe4cf6ec829b7609d88d1a1e2a5b
Joseph Parker <jparker010190@gmail.com>
2018-04-03 01:39:18 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, 1, "brown", "hot", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, 1, "brown", "hot", 1);
