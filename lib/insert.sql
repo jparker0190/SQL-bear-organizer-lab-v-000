e88c72cce4bf0d03b6a6be4ba17facca52de5464
Joseph Parker <jparker010190@gmail.com>
2018-04-03 01:39:48 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Ch
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, 1, "brown", "hot", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, 1, "brown", "hot", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, 1, "brown", "hot", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, 1, "brown", "hot", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, 1, "brown", "hot", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, 1, "brown", "hot", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 1, 1, "brown", "hot", 1);
